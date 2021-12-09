fee24ff2815478bd833dc449810c315b61a39116
supreme <nurhassan696@gmail.com>
2021-12-09 15:13:46 -0600

    aassigned the values to the keys of the object

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,24 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+
+function addToCollection(title, artist, yearPublished) {
+   console.log('in addToCollection:', title, artist, yearPublished);
+   let greatestAlbum = {
+      title: title,
+      artist: artist,
+      yearPublished: yearPublished
+   }
+   collection.push(greatestAlbum);
+   return greatestAlbum;
+
+
+}
+
+console.log('running in:', addToCollection('Victory Lap', 'Nipsey Hussle', 2018));
+
+// console.log('running in:', addToCollection('Victory Lap 2', 'Nipsey Hussle', 2021));
+
+
+console.log(collection);
+
