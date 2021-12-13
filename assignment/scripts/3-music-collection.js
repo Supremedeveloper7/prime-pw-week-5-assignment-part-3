8bca359453b3ba053e1cac51cc8ee315a48263df
supreme <nurhassan696@gmail.com>
2021-12-12 20:13:49 -0600

    submitting part 3 wk 5

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -16,9 +16,26 @@ function addToCollection(title, artist, yearPublished) {
 }
 
 console.log('running in:', addToCollection('Victory Lap', 'Nipsey Hussle', 2018));
-
-// console.log('running in:', addToCollection('Victory Lap 2', 'Nipsey Hussle', 2021));
-
-
+console.log('running in:', addToCollection('The Marathon', 'Nipsey Hussle', 2010));
+console.log('running in:', addToCollection('The Marathon Continues', 'Nipsey Hussle', 2011));
+console.log('running in:', addToCollection('Born Sinner', 'J cole', 2013));
+console.log('running in:', addToCollection('Mailbox Money', 'Nipsey Hussle', 2014));
+console.log('running in:', addToCollection('Stillmatic', 'Nas', 2001));
 console.log(collection);
 
+function findByArtist(artist){
+   let find = []
+   for (let i=0; i<collection.length; i++){
+      if(collection[i].artist=== artist){
+      find.push(collection[i])
+      }
+   }
+   return find;
+}
+
+console.log( 'running in:', findByArtist('J cole'));
+console.log( 'running in:', findByArtist('Nas'));
+console.log( 'running in:', findByArtist('Nipsey Hussle'));
+console.log( 'running in:', findByArtist('Young Dolph'));
+
+console.log(collection);
